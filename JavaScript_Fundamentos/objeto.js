09f88607e616d8ddacaa6fd91037dd9049c776cb
Maxel Udson Alves da Conceição <maxellopes32@gmail.com>
2021-04-23 17:26:41 -0300

    Object em JS

---
+++ JavaScript_Fundamentos/objeto.js
@@ -0,0 +1,12 @@
+const prod1 = {} //Representa um objeto, objetos são coleções de chaves valor
+prod1.nome = 'Celular Ultra Mega';
+prod1.preco = 4998.90;
+
+console.log(prod1);
+
+const prod2 = {
+    nome: 'Camisa Polo',
+    preco: 79.90
+}
+
+console.log(prod2);
