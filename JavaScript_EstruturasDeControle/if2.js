3886e2804f8907e81e59c11acaacfd535a13ff33
Maxel Udson Alves da Conceição <maxellopes32@gmail.com>
2021-05-03 16:25:00 -0300

    Usando IF parte 2

---
+++ JavaScript_EstruturasDeControle/if2.js
@@ -0,0 +1,20 @@
+function teste1(num)
+{
+    if(num > 7)
+        console.log(num);
+    console.log('final');
+}
+
+teste1(8);
+teste1(6);
+
+function teste2(num)
+{
+    if(num > 7);
+    {
+        console.log(num);
+    }
+}
+
+teste2(8);
+teste2(6);
