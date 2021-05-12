c6e597f749157518a1f33b992a08b6823c09c848
Maxel Udson Alves da Conceição <maxellopes32@gmail.com>
2021-05-12 16:34:30 -0300

    Tipos de declarações de funções

---
+++ JavaScript_Função/tiposDeDeclaração.js
@@ -0,0 +1,16 @@
+//Function declaration, pode ser invocada antes da linha de declaração da função
+function soma(a, b)
+{
+    return a + b;
+}
+
+//Function expression
+const sub = function(a, b) {
+    return a - b;
+}
+
+//Named function expression
+const mult = function mult(a, b) {
+    return a * b;
+}
+
