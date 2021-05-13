6f5e5acfc0e3d19af938093558b26b498b40d7cd
Maxel Udson Alves da Conceição <maxellopes32@gmail.com>
2021-05-13 17:14:23 -0300

    Função factory parte 2

---
+++ JavaScript_Função/factory2.js
@@ -0,0 +1,11 @@
+function criarProduto(nome, preco, desconto = 0.1)
+{
+    return {
+        nome,
+        preco,
+        desconto
+    }
+}
+
+console.log(criarProduto('Notebook', 2199.49));
+console.log(criarProduto('IPad', 1999.49, 0.2));
