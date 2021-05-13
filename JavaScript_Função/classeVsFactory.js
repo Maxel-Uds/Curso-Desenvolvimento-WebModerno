5f05f184b7647b3a8a6d7ba1928e834d982ad827
Maxel Udson Alves da Conceição <maxellopes32@gmail.com>
2021-05-13 17:28:22 -0300

    Classe vs Factory

---
+++ JavaScript_Função/classeVsFactory.js
@@ -0,0 +1,26 @@
+//Classe
+class Pessoa
+{
+    constructor(nome)
+    {
+        this.nome = nome
+    }
+
+    falar()
+    {
+        console.log(`Meu nome é ${this.nome}`);
+    }
+}
+
+const p1 = new Pessoa('joão');
+p1.falar();
+
+//Factory
+const criarPessoa = nome => {
+    return {
+        falar: () => console.log(`Meu nome é ${nome}`)
+    }
+}
+
+const p2 = criarPessoa('joão');
+p2.falar();
