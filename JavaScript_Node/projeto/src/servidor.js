303f0f159776d92b7899273219fb61cc885cc00a
Maxel Udson Alves da Conceição <maxellopes32@gmail.com>
2021-05-28 15:16:48 -0300

    Projeto: API com Express parte 2

--- JavaScript_Node/projeto/src/servidor.js
+++ JavaScript_Node/projeto/src/servidor.js
@@ -1,9 +1,23 @@
 const porta = 3003;
 const express = require('express');
 const app = express();
+const bancoDeDados = require('./bancoDeDados');
 
 app.get('/produtos', (req, res, next) => { //O primeiro parâmetro é a URL
-    res.send({ nome: 'Notebook', preco: 123.45 }); //Vai converter para JSON
+    res.send(bancoDeDados.getProdutos());
+});
+
+app.get('/produtos/:id', (req, res, next) => { //Os ':' antes do id indica que ele um parâmetro que está sendo enviado pela URL
+    //O req.params é o local onde ficam guardados os parâmetros enviados na URL que podem ser acessados através da notação ponto
+    res.send(bancoDeDados.getProduto(req.params.id));
+});
+
+app.post('/produtos', (req, resp, next) => {
+    const produto = bancoDeDados.salvarProduto({
+        nome: req.body.name,
+        preco: req.body.preco
+    });
+    resp.send(produto); //Converte para JSON
 });
 
 app.listen(porta, () => {
