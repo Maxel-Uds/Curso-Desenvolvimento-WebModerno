c76c83e6a1a6c22abe339fc82d1c466edc584606
Maxel-Uds <maxellopes32@gmail.com>
2021-11-05 16:08:42 -0300

    XMLHttpRequest parte 2

--- AJAX/server.js
+++ AJAX/server.js
@@ -7,5 +7,26 @@ app.use(express.static('.'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+const multer = require('multer');
+const storage = multer.diskStorage({
+    destination: function(req, file, callback) {
+       callback(null, './upload'); 
+    },
+    filename: function(req, file, callback) {
+        callback(null, `${Date.now()}_${file.originalname}`);
+    }
+});
+const upload = multer({ storage }).single('arquivo');
+
 app.get("/dados/estados.json");
+app.post('/upload', (req, res) => {
+   upload(req, res, err => {
+        if(err) {
+            return res.end('Ocorreu um erro')
+        }
+
+        res.end('Concluído com sucesso');
+   });
+});
+
 app.listen(8080, () => console.log('Executando...'));
