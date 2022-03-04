bb43a12891be08894bf0ae5af7878963ca0f0c78
Maxel-Uds <maxellopes32@gmail.com>
2022-03-04 15:46:45 -0300

    Fundamentos de Gulp parte 2

--- GULP/basico/gulpfile.js
+++ GULP/basico/gulpfile.js
@@ -1,9 +1,30 @@
 const gulp = require('gulp');
-const { series } = require('gulp');
+const { series, parallel } = require('gulp');
 
-function copiar(callback) {
-    console.log('Tarefa de copiar!');
+const antes1 = callback => {
+    console.log('Trefa Antes 1!');
     return callback();
 }
 
-module.exports.default = series(copiar);
+const antes2 = callback => {
+    console.log('Trefa Antes 2!');
+    return callback();
+}
+
+function copiar(callback) {
+    // gulp.src(['pastaA/arquivo1.txt', 'pastaA/arquivo2.txt'])
+    gulp.src('pastaA/**/*.txt')
+        .pipe(gulp.dest('pastaB'));
+    return callback();
+}
+
+const fim = callback => {
+    console.log('Tarefa Fim!');
+    return callback();
+}
+
+module.exports.default = series(
+    parallel(antes1, antes2),
+    copiar,
+    fim,
+);
