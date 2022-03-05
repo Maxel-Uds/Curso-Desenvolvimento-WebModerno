af699de26dbecc90a3ee36058aff1d04e54650e8
Maxel-Uds <maxellopes32@gmail.com>
2022-03-05 15:50:03 -0300

    Processando SASS com GULP parte 1

---
+++ GULP/css/gulpfile.js
@@ -0,0 +1,17 @@
+const { series } = require('gulp');
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const uglifyCss = require('gulp-uglifycss');
+const concat = require('gulp-concat');
+
+function transformacaoCSS() {
+    return gulp.src('src/sass/index.scss')
+        .pipe(sass().on('error', sass.logError ))
+        .pipe(uglifyCss({
+            "uglifyComments": true
+        }))
+        .pipe(concat('estilo.min.css'))
+        .pipe(gulp.dest('build/css'));
+}
+
+exports.default = series(transformacaoCSS);
