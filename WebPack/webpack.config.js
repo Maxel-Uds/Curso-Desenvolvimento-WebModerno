3a9a5158dc291c7f78f0796ecde8da68357252e7
Maxel-Uds <maxellopes32@gmail.com>
2022-03-07 13:23:40 -0300

    Atualização de dependências

--- WebPack/webpack.config.js
+++ WebPack/webpack.config.js
@@ -1,8 +1,8 @@
 const modoDev = process.env.NODE_ENV !== 'production';
 const webpack = require('webpack');
 const miniCssExtract = require('mini-css-extract-plugin');
-const uglifyJsPlugin = require('uglifyjs-webpack-plugin');
 const optimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin')
+const TerserPlugin = require('terser-webpack-plugin')
 
 module.exports = {
     mode: modoDev ? 'development' : 'production',
@@ -13,9 +13,11 @@ module.exports = {
     },
     optimization: {
         minimizer: [
-            new uglifyJsPlugin({
-                cache: true,
-                parallel: true
+            new TerserPlugin({
+                parallel: true,
+                terserOptions: {
+                    ecma: 6,
+                },
             }),
             new optimizeCSSAssetsPlugin({})
         ]
